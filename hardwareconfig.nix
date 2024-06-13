1741d736f3081fab813a0871a7ebf3445714b882
sonicpanther101 <sonicpanther101@gmail.com>
2024-06-13 17:52:14 +1200

    nouveau didn't work

--- hardwareconfig.nix
+++ hardwareconfig.nix
@@ -10,50 +10,17 @@
     #------------------------------------------------------------
     # [4070]
 
-# normal setup (flickers on vivaldi and minecraft)
-  #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-#  hardware.opengl = {
-#    enable = true;
-#    driSupport = true;
-#    driSupport32Bit = true;
-#  };
-#  services.xserver.videoDrivers = ["nvidia"];
-#  hardware.nvidia = {
-#    modesetting.enable = true;
-#    nvidiaSettings = true;
-#  };
-
-
-# Nouveau setup hopefully no flickering
-# Ensure the Nouveau module is loaded
-  boot.kernelModules = [ "nouveau" ];
-
-  # Blacklist the proprietary NVIDIA driver, if needed
-  boot.blacklistedKernelModules = [ "nvidia" "nvidia_uvm" "nvidia_drm" "nvidia_modeset" ];
-
-  # Configure Xorg to use the Nouveau driver
-  services.xserver = {
-    enable = true;
-    videoDrivers = [ "nouveau" ];
-
-    # Optional: Configure display settings, if necessary
-    # displayManager = {
-    #   ...
-    # };
-    # desktopManager = {
-    #   ...
-    # };
-  };
-
-  # Enable hardware acceleration for Nouveau
+  #normal setup (flickers on vivaldi and minecraft)
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   hardware.opengl = {
     enable = true;
-    extraPackages = with pkgs; [
-      # Add packages needed for Nouveau acceleration here
-      # For example, Mesa for OpenGL:
-      mesa
-      mesa.drivers
-    ];
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
   };
 
 
