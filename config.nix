ac97b4714dc12d28b421911b7b5e0fe9681df718
sonicpanther101 <116870775+sonicpanther101@users.noreply.github.com>
2024-06-17 22:12:37 +1200

    Update config.nix

--- config.nix
+++ config.nix
@@ -5,37 +5,63 @@
   modulesPath,
   options,
   ...
-}:
-{
-  environment.systemPackages = with pkgs; [
-    neofetch
-    git
-    wget
-    usbutils
-    alejandra
-    spotify
-    prismlauncher
-    jre8
-    mangohud
-    bottles
-    #hyprland
-  ];
+}: {
+  #------------------------------------------------------------
+  # [4070]
 
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-  programs.gamemode.enable = true;
+  #normal setup (flickers on vivaldi and minecraft)
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "555.52.04";
+    sha256_64bit = "sha256-nVOubb7zKulXhux9AruUTVBQwccFFuYGWrU1ZiakRAI=";
+    sha256_aarch64 = "sha256-Kt60kTTO3mli66De2d1CAoE3wr0yUbBe7eqCIrYHcWk=";
+    openSha256 = "sha256-wDimW8/rJlmwr1zQz8+b1uvxxxbOf3Bpk060lfLKuy0=";
+    settingsSha256 = "sha256-PMh5efbSEq7iqEMBr2+VGQYkBG73TGUh6FuDHZhmwHk=";
+    persistencedSha256 = "sha256-KAYIvPjUVilQQcD04h163MHmKcQrn2a8oaXujL2Bxro=";
+  };
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    open = false;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+  };
 
-  services.xserver.enable = false;
-  services.displayManager.sddm.wayland.enable = true;
+  #------------------------------------------------------------
+  # [B550]
 
-  # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
-  #services.xserver.desktopManager.default = "gnome";
+  systemd.services.bugfixSuspend-GPP0 = {
+    enable = lib.mkDefault true;
+    description = "Fix crash on wakeup from suspend/hibernate (b550 bugfix)";
+    unitConfig = {
+      Type = "oneshot";
+    };
+    serviceConfig = {
+      User = "root"; # root may not be necessary
+      # check for gppN, disable if enabled
+      # lifted from  https://www.reddit.com/r/gigabyte/comments/p5ewjn/comment/ksbm0mb/ /u/Demotay
+      ExecStart = "-${pkgs.bash}/bin/bash -c 'if grep 'GPP0' /proc/acpi/wakeup | grep -q 'enabled'; then echo 'GPP0' > /proc/acpi/wakeup; fi'";
+      RemainAfterExit = "yes"; # required to not toggle when `nixos-rebuild switch` is ran
+    };
+    wantedBy = ["multi-user.target"];
+  };
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  services.flatpak.enable = true;
+  systemd.services.bugfixSuspend-GPP8 = {
+    enable = lib.mkDefault true;
+    description = "Fix crash on wakeup from suspend/hibernate (b550 bugfix)";
+    unitConfig = {
+      Type = "oneshot";
+    };
+    serviceConfig = {
+      User = "root";
+      ExecStart = "-${pkgs.bash}/bin/bash -c 'if grep 'GPP8' /proc/acpi/wakeup | grep -q 'enabled'; then echo 'GPP8' > /proc/acpi/wakeup; fi''";
+      RemainAfterExit = "yes";
+    };
+    wantedBy = ["multi-user.target"];
+  };
 }
